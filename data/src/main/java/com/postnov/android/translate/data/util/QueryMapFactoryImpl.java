a5bb484395b571a3a823cb1bff7337428d1395e6
v.postnov <valpostnov@gmail.com>
2017-04-02 21:52:34 +0300

    update QueryMapFactory

--- data/src/main/java/com/postnov/android/translate/data/util/QueryMapFactoryImpl.java
+++ data/src/main/java/com/postnov/android/translate/data/util/QueryMapFactoryImpl.java
@@ -1,16 +1,19 @@
 package com.postnov.android.translate.data.util;
 
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 
 import com.postnov.android.translate.domain.QueryMapFactory;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 import javax.inject.Inject;
 
-import static com.postnov.android.translate.data.util.PreferencesManager.PREF_LANG_1_CODE;
-import static com.postnov.android.translate.data.util.PreferencesManager.PREF_LANG_2_CODE;
+import static com.postnov.android.translate.data.util.LanguageHelperImpl.EN;
+import static com.postnov.android.translate.data.util.LanguageHelperImpl.PREF_LANG_1_CODE;
+import static com.postnov.android.translate.data.util.LanguageHelperImpl.PREF_LANG_2_CODE;
+import static com.postnov.android.translate.data.util.LanguageHelperImpl.RU;
 
 /**
  * @author Valentin Postnov
@@ -19,19 +22,21 @@ public class QueryMapFactoryImpl implements QueryMapFactory {
 
     public static final String LANG = "lang";
     public static final String TEXT = "text";
+
     private static final String DICT_KEY = "dict";
     private static final String TRANS_KEY = "trns";
     private static final String KEY = "key";
-    private final PreferencesManager preferencesManager;
+
+    private final SharedPreferences preferences;
     private final Map<String, String> dictQueryMap;
     private final Map<String, String> transQueryMap;
 
     @Inject
-    public QueryMapFactoryImpl(PreferencesManager preferencesManager) {
-        this.preferencesManager = preferencesManager;
-        dictQueryMap = new TreeMap<>();
+    public QueryMapFactoryImpl(SharedPreferences preferences) {
+        this.preferences = preferences;
+        dictQueryMap = new HashMap<>(3);
         dictQueryMap.put(KEY, DICT_KEY);
-        transQueryMap = new TreeMap<>();
+        transQueryMap = new HashMap<>(3);
         transQueryMap.put(KEY, TRANS_KEY);
     }
 
@@ -50,7 +55,7 @@ public class QueryMapFactoryImpl implements QueryMapFactory {
     }
 
     private String createLangPath() {
-        return preferencesManager.getString(PREF_LANG_1_CODE, "ru") + "-"
-                + preferencesManager.getString(PREF_LANG_2_CODE, "en");
+        return preferences.getString(PREF_LANG_1_CODE, RU) + "-"
+                + preferences.getString(PREF_LANG_2_CODE, EN);
     }
 }
